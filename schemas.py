063b353d39b77d42efc34533de95801364f3b781
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2026-04-21 21:53:54 +0100

    refactor(schemas): add country_name and other extra fields in profile models

--- schemas.py
+++ schemas.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 from typing import Optional, List
+from datetime import datetime
 
 class ProfileSchema(BaseModel):
     id: str
     name: str
     gender: Optional[str]
     gender_probability: Optional[float]
-    sample_size: Optional[int]
     age: Optional[int]
     age_group: Optional[str]
     country_id: Optional[str]
+    country_name: Optional[str]
+    country_probability: Optional[float]
+    created_at: Optional[datetime]
+
 
     class Config:
         from_attributes = True
